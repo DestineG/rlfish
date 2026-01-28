9481002c3aaa9eea955f54f9389fa373f5ddf631
six <destinefut@gmail.com>
2026-01-28 08:34:15 +0800

    feat: 第六章内容完成

--- chapter_6/Expectation-based_Qlearning.py
+++ chapter_6/Expectation-based_Qlearning.py
@@ -191,39 +191,21 @@ class Agent:
         self.gama = 0.9
         self.action_size = 4
 
-        default_policy = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
-        self.pi = defaultdict(lambda: default_policy.copy())
-        self.b = defaultdict(lambda: default_policy.copy())
         self.Q = defaultdict(lambda: 0.0)
-        self.memory = deque(maxlen=2)
     
     def get_action(self, state):
-        # 行为策略
-        action_probs = self.b[state]
+        action_probs = greedy_probs(self.Q, state, self.action_size, epsilon=0.1)
         actions = list(action_probs.keys())
         probs = list(action_probs.values())
         return np.random.choice(actions, p=probs)
-    
-    def reset_memory(self):
-        self.memory.clear()
-    
-    # 策略更新
-    def update(self, state, action, reward, done):
-        self.memory.append((state, action, reward, done))
-        if len(self.memory) < 2:
-            return
-        state, action, reward, done = self.memory[0]
-        next_state, next_action, _, _ = self.memory[1]
-        next_q = 0 if done else self.Q[(next_state, next_action)]
-        # 重要性采样
-        rho = 1 if done else self.pi[next_state][next_action] / self.b[next_state][next_action]
-        target = rho * (reward + self.gama * next_q)
-        self.Q[(state, action)] += 0.1 * (target - self.Q[(state, action)])
 
-        # 目标策略更新
-        self.pi[state] = greedy_probs(self.Q, state, self.action_size, epsilon=0)
-        # 行为策略更新
-        self.b[state] = greedy_probs(self.Q, state, self.action_size, epsilon=0.1)
+    # 策略更新
+    def update(self, state, action, reward, next_state, done):
+        
+        next_q = 0 if done else (max([self.Q[(next_state, next_action)] for next_action in range(self.action_size)]))
+
+        target = (reward + self.gama * next_q)
+        self.Q[(state, action)] += 0.1 * (target - self.Q[(state, action)])
 
 def td_eval():
     env = GridWorld()
@@ -239,7 +221,7 @@ def td_eval():
             action = agent.get_action(state)
             next_state, reward, done = env.step(action)
             # 实时更新
-            agent.update(state, action, reward, done)
+            agent.update(state, action, reward, next_state, done)
             state = next_state
 
     # 提取状态价值函数 V 和 策略 pi
